ff88b26207411b9a4410ec68b585b48d22493834
Xander <xanderpalmer7@gmail.com>
2024-03-18 21:49:19 -0400

    Added the routes to the main component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { BuilderComponent } from './builder/builder.component';
+import { ViewerComponent } from './viewer/viewer.component';
+import { ValidaterComponent } from './validater/validater.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'build', component: BuilderComponent },
+    { path: 'view', component: ViewerComponent },
+    { path: 'validate', component: ValidaterComponent },
+    { path: '', component: HomeComponent }
+];
