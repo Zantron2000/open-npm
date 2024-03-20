1f41bdafb9b3745e75b9a2b491e303d9bd568dc9
Xander <xanderpalmer7@gmail.com>
2024-03-19 20:03:55 -0400

    Added a header modal for small screen width

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  links = [
+    { path: '/build', label: 'Build' },
+    { path: '/view', label: 'View' },
+    { path: '/validate', label: 'Validate' }
+  ];
+  showModal = false;
 
+  toggleModal() {
+    this.showModal = !this.showModal;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '3/10': '30%',
+        '3/4': '75%',
+        '4/5': '80%',
+        '9/10': '90%',
+      }
+    },
   },
   plugins: [],
 }
