cace6f1c026b30cc96285f52c27a83b3add6c558
Xander <xanderpalmer7@gmail.com>
2024-03-23 14:41:47 -0400

    Added type declarations and a starter page

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -1,6 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+type Category = {
+  name: string;
+  items: {
+    name: string;
+    editting: boolean;
+  }[];
+};
+
+type SortMethod = {
+  name: string;
+  active: boolean;
+};
+
 @Component({
   selector: 'app-builder',
   standalone: true,
@@ -9,41 +22,9 @@ import { Component } from '@angular/core';
   styleUrl: './builder.component.css'
 })
 export class BuilderComponent {
-  categories = [
-    {
-      name: 'Category 1',
-      items: [
-        { name: 'Item 1' },
-        { name: 'Item 2' },
-        { name: 'Item 3' },
-      ]
-    },
-    {
-      name: 'Category 2',
-      items: [
-        { name: 'Item 4' },
-        { name: 'Item 5' },
-        { name: 'Item 6' },
-      ]
-    },
-    {
-      name: 'Category 3',
-      items: [
-        { name: 'Item 7' },
-        { name: 'Item 8' },
-        { name: 'Item 9' },
-      ]
-    },
-    {
-      name: 'Category 4',
-      items: [
-        { name: 'Item 10', editting: true },
-        { name: 'Item 11' },
-        { name: 'Item 12' },
-      ]
-    }
-  ];
-  sortMethods = [
+  categories: Category[] = [];
+
+  sortMethods: SortMethod[] = [
     { name: 'Types', active: true },
     { name: 'Tags', active: false },
   ];
