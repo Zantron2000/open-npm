ec70de3ce6d85197da970e9e71f0dd117e3b53a2
Xander <xanderpalmer7@gmail.com>
2024-03-23 11:47:45 -0400

    Hooked nav bar display to components

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -9,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrl: './builder.component.css'
 })
 export class BuilderComponent {
-
+  categories = [
+    {
+      name: 'Category 1',
+      items: [
+        { name: 'Item 1' },
+        { name: 'Item 2' },
+        { name: 'Item 3' },
+      ]
+    },
+    {
+      name: 'Category 2',
+      items: [
+        { name: 'Item 4' },
+        { name: 'Item 5' },
+        { name: 'Item 6' },
+      ]
+    },
+    {
+      name: 'Category 3',
+      items: [
+        { name: 'Item 7' },
+        { name: 'Item 8' },
+        { name: 'Item 9' },
+      ]
+    },
+    {
+      name: 'Category 4',
+      items: [
+        { name: 'Item 10', editting: true },
+        { name: 'Item 11' },
+        { name: 'Item 12' },
+      ]
+    }
+  ];
+  sortMethods = [
+    { name: 'Types', active: true },
+    { name: 'Tags', active: false },
+  ];
 }
