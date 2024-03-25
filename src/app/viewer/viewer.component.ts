75225d93d43c08cb8c5af19b1be62483c93aefff
luci <lucille_blain@live.com>
2024-03-24 21:39:02 -0400

    added code editor

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import { CodeModel } from '@ngstack/code-editor';
+import { CodeEditorModule } from '@ngstack/code-editor';
 @Component({
   selector: 'app-viewer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,CodeEditorModule],
   templateUrl: './viewer.component.html',
-  styleUrl: './viewer.component.css'
+  styleUrls: ['./viewer.component.css']
 })
 export class ViewerComponent {
+  theme = 'vs-dark';
 
+  model: CodeModel = {
+    language: 'json',
+    uri: 'main.json',
+    value: '{}'
+  };
+
+  options = {
+    contextmenu: true,
+    minimap: {
+      enabled: true
+    }
+  };
+
+  // Remove npm package dependencies and only keep Angular module dependencies
+  dependencies: string[] = [
+    '@angular/core',
+    '@angular/common',
+    '@ngstack/code-editor'
+  ];
 }
