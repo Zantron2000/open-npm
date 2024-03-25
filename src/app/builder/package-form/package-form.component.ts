2a998f986e0d69e3446c64d610b248d1846a4774
Xander <xanderpalmer7@gmail.com>
2024-03-24 22:06:55 -0400

    Built method to submit a package creation

--- src/app/builder/package-form/package-form.component.ts
+++ src/app/builder/package-form/package-form.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+type Package = {
+  name: string;
+  description: string;
+  version: string;
+};
 
 @Component({
   selector: 'app-package-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './package-form.component.html',
   styleUrl: './package-form.component.css'
 })
 export class PackageFormComponent {
-  package = {
-    name: '',
-    description: '',
-    version: ''
-  };
+  @Output() packageCreated = new EventEmitter<Package>();
+  @Input() package: Package = { name: '', description: '', version: '' };
+  submitted = false;
+
+  submit(form: NgForm) {
+    this.submitted = true;
+
+    if (form.valid) {
+      this.packageCreated.emit(this.package);
+      this.submitted = false;
+    }
+  }
 }
