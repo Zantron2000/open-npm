9a21ac4aeb65c30501f86e03d542a4cca1ed1815
Xander <xanderpalmer7@gmail.com>
2024-03-25 21:11:32 -0400

    Created the forms for type forms

--- src/app/builder/type-form/type-form.component.ts
+++ src/app/builder/type-form/type-form.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-type-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './type-form.component.html',
   styleUrl: './type-form.component.css'
 })
 export class TypeFormComponent {
-
+  code = 'asdf\nasdf\nasdf\n'
 }
