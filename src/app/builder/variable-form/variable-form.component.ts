7710842c99dd4cd120ad6e4f7865e3d87b144d5b
Xander <xanderpalmer7@gmail.com>
2024-03-28 10:54:38 -0400

    Created initial variable functions

--- src/app/builder/variable-form/variable-form.component.ts
+++ src/app/builder/variable-form/variable-form.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+type Variable = {
+  name: string;
+  import: string;
+  github: string;
+  description: string;
+  type: string;
+  value: string;
+  examples: string[];
+};
 
 @Component({
   selector: 'app-variable-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './variable-form.component.html',
   styleUrl: './variable-form.component.css'
 })
 export class VariableFormComponent {
+  @Output() variableCreated = new EventEmitter<Variable>();
+  @Output() cancelUpdates = new EventEmitter();
+  @Input() currentVariable: Variable = {
+    name: '', import: '', github: '', description: '',
+    value: '', type: '', examples: []
+  };
+  @Input() newInstance: Boolean = true;
+  errors: { [key: string]: string } = {};
+  variable: Variable = JSON.parse(JSON.stringify(this.currentVariable));
 
+  ngOnInit() {
+    this.variable = this.newInstance ? this.variable : JSON.parse(JSON.stringify(this.currentVariable));
+  }
+
+  adjustHeight(target: EventTarget | null) {
+    const textarea = target as HTMLTextAreaElement;
+
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  }
+
+  submit(form: NgForm) {
+    let hasErrors = false;
+
+    if (!hasErrors) {
+      this.variableCreated.emit(this.variable);
+    }
+  }
+
+  cancelChanges() {
+    this.cancelUpdates.emit();
+  }
 }
